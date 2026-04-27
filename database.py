b74a460bd6d79051cd6ac9b53cf849eeb3b61193
zaid-issaTech <202210584@ammanu.edu.jo>
2026-04-27 15:00:30 +0300

    Add env config for database URL

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/skills_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
